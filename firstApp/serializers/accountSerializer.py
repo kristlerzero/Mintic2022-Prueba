1778a522a3ca9fa90c0ff3e9ca406be380264d74
mefisto19 <sergiobeltranpatino@gmail.com>
2022-09-18 21:38:04 -0500

    CommitNumber1

---
+++ firstApp/serializers/accountSerializer.py
@@ -0,0 +1,8 @@
+
+from firstApp.models.account import Account
+from rest_framework import serializers
+
+class AccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = ['balance', 'lastChangeDate', 'isActive']
